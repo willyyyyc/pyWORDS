6e2c46b5b2dfe4a4a3866152acdcd8e06638985f
willyyyyc <willcarmichaelcf@gmail.com>
2023-11-09 20:08:03 -0400

    Refactoring; changed variable names and comments.

--- get_def.py
+++ get_def.py
@@ -16,7 +16,7 @@ try:
 except NoEntry:
     print("Could not find matching entry in dictionary.")
 else:
-
+    # The parts of speach in the english language.
     all_parts = [
         'Noun', 
         'Verb', 
@@ -27,25 +27,35 @@ else:
         'Interjection'
         ]
 
-    forms = []
+    # Loop through all headers in dictionary entry that 
+    # contain a part of speach, adding to list of forms for that
+    # word if not already there - repeats mean it is a different
+    # language. Ex: First "Noun" header is in the English section,
+    # which is what we want. Next "Noun" is in a different language
+    # section, i.e., "French" or "Old English".
+    # There are no sections separating languages, so this is a work-
+    # around for determining when to stop scanning document.
+    unique_forms = []
+    defintion = []
     for form in soup.select('span.mw-headline'):
-        if form.text in all_parts and form.text not in forms:
-            forms.append(form.text)
-            block = [form.text]
+        if form.text in all_parts and form.text not in unique_forms:
+            unique_forms.append(form.text)
+            section = [form.text]
             elem = form.parent
             while True:
                 if elem.name == 'p':
                     print(elem.text)
-                    block.append(elem.text)
+                    section.append(elem.text)
                 if elem.name == 'ol':
                     list_of_def = {
                         'list': elem
                     }
-                    block.append(list_of_def)
-                    forms.append(block)
+                    section.append(list_of_def)
+                    defintion.append(section)
                     break
                 else:
                     elem = elem.next_sibling.next_sibling
   
-
+# remove all children from dom or <dl>
+print()
     
